b41011eb635193971b128bd847c90501941d3a85
Kevin Moore <github@j832.com>
2014-11-08 01:08:17 +0100

    updates to memcache sample
    R=kustermann@google.com
    
    Review URL: https://chromereviews.googleplex.com/114977013

--- memcache/bin/server.dart
+++ memcache/bin/server.dart
@@ -2,12 +2,11 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:io';
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:appengine/appengine.dart';
-import 'package:memcache/memcache.dart';
-
 import 'package:mustache/mustache.dart' as mustache;
 
 final _MAIN_PAGE = mustache.parse("""
@@ -16,60 +15,79 @@ final _MAIN_PAGE = mustache.parse("""
     <title>Main page</title>
     <style>
       div {
-        padding: 10%;
+        padding: 24px;
       }
     </style>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.0/css/bootstrap.min.css">
 </head>
   </head>
   <body>
     <div>
-      <h2>Add new user:</h2>
-      <form name="input" action="/mustache/" method="get">
+      <form name="input" method="get">
         <input type="text" name="user">
-        <input type="submit" value="Submit">
+        <input type="submit" value="Create user">
+      </form>
+      <form name="input" action='/clear' method="get">
+        <input type="submit" value="Clear all users">
       </form>
   
-      <h2>Existing Users:</h2>
+      <h2>Users:</h2>
       <ul>
         {{#users}}
           <li>{{user}}</li>
         {{/users}}
       </ul>
+      
     </div>
   </body>
 </html>
 """);
 
-void _sendResponse(HttpResponse response, int statusCode, String message,
-             {bool isHtml: false}) {
+Future _sendResponse(HttpResponse response, int statusCode, String message) {
   var data = UTF8.encode(message);
-  response.headers.contentType =
-      new ContentType('text', isHtml ? 'html' : 'plain', charset: 'utf-8');
-  response.headers.set("Cache-Control", "no-cache");
-  response.statusCode = statusCode;
-  response.contentLength = data.length;
-  response.add(data);
-  response.close();
+  response
+      ..headers.contentType = new ContentType('text', 'html', charset: 'utf-8')
+      ..headers.set("Cache-Control", "no-cache")
+      ..statusCode = statusCode
+      ..contentLength = data.length
+      ..add(data);
+  return response.close();
 }
 
 void _serveMustache(HttpRequest request) {
-  Memcache memcache = context.services.memcache;
+  var logging = context.services.logging;
+  var memcache = context.services.memcache;
+  var users = [];
 
-  request.drain().then((_) {
+  memcache.get('users').then((encodedUsers) {
+    if (encodedUsers == null) return;
+    try {
+      users = JSON.decode(encodedUsers);
+    } catch (err) {
+      logging.error("Error when decoding '$encodedUsers':\n$err");
+    }
+  }).then((_) {
+    if (request.uri.path == '/clear') {
+      return memcache.remove('users').then((_) {
+        // redirect back to the root of the application
+        return request.response.redirect(new Uri(path: '/'));
+      });
+    }
+
     var newUser = request.uri.queryParameters['user'];
 
-    Map users = {'users' : []};
-    memcache.get('users').then((String encodedUsers) {
-      users = JSON.decode(encodedUsers);
-    }).catchError((_){}).whenComplete(() {
-      if (newUser != null) users['users'].add({'user': newUser});
+    if (newUser != null && newUser.trim().isNotEmpty) {
+      users.add({'user': newUser});
 
-      memcache.set('users', JSON.encode(users)).then((_) {
-        var buffer = new StringBuffer();
-        _MAIN_PAGE.render(users, buffer);
-        _sendResponse(request.response, HttpStatus.OK, "$buffer", isHtml: true);
+      // add the new value to memcache
+      return memcache.set('users', JSON.encode(users)).then((_) {
+        // redirect back to the root of the application
+        return request.response.redirect(new Uri(path: '/'));
       });
-    });
+    } else {
+      var body = _MAIN_PAGE.renderString({'users': users});
+      return _sendResponse(request.response, HttpStatus.OK, body);
+    }
   });
 }
 
